b42ad1d0927ccb26aae9661818e1ad1f5685d1a1
Aldo Pramilhiawan <aldopramilhiawan0@gmail.com>
2022-11-09 10:44:59 +0700

    push aldo

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import usersReducer from './features/users/userSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./features/users/userSlice";
 
 export const store = configureStore({
   reducer: {
-    users: usersReducer
+    users: usersReducer,
   },
-})
+});
